36e1b06aad17228ed7b1aefb00a766a90467b906
tongyu110 <pengchengxue110@163.com>
2018-04-09 16:55:49 +0800

    init

---
+++ Test/server.js
@@ -0,0 +1,27 @@
+var http = require('http');
+
+http.createServer(TestPrintJson).listen(8888);
+
+// 终端打印如下信息
+console.log('Server running at http://127.0.0.1:8888/');
+
+
+function TestPrintJson(request, response) {
+
+	let data = {'name':'abu','age':30};
+
+	response.writeHead(200,{'Content-Type':'application/json'});
+	response.end(JSON.stringify(data));
+
+}
+
+
+function TestHelloWorld(request, response) {
+	 // 发送 HTTP 头部 
+    // HTTP 状态值: 200 : OK
+    // 内容类型: text/plain
+    response.writeHead(200, {'Content-Type': 'text/plain'});
+	
+    // 发送响应数据 "Hello World"
+    response.end('Hello World\n');
+}
